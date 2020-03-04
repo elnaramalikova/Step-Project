4d431d74b38035b13c64ef97ae17ad4031996603
unknown <melikova.elnara999@gmail.com>
2020-03-04 15:58:06 +0400

    project

--- js/main.js
+++ js/main.js
@@ -21,60 +21,25 @@ function selectTab(e, tabId) {
 
 };
 
-//LOAD MORE
-// var btn = document.getElementById('loadmore');
-// btn.addEventListener('click', function(e){
-// document.querySelector(".more").style.display='flex';
-// btn.style.display='none';
-// });
+// LOAD MORE
+var btn = document.getElementById('loadmore');
+btn.addEventListener('click', function(e){
+document.querySelector(".more").style.display='flex';
+btn.style.display='none';
+});
 
-// var buttons=document.getElementsByClassName('tab')[0];
-// var tabimg=document.getElementsByClassName('tabimg')[0];
-
-// buttons.addEventListener('click', function(){
-//     for(let j=0; j<buttonvalue.length,tabimg.length; j++){
-//         var buttonvalue =buttons[i].getAttribute('data-filter');
-//         console.log(j);
-//         if(buttonvalue[j] == tabimg[i].className){
-//             buttonvalue[j].classList.add('active');
-//             tabimg[i].style.display="block";
-//         }
-//         else{
-//             buttonvalue[j].classList.remove('active');
-//             tabimg[i].style.display='none'
-//         }
-//         console.log(tabimg)
-    
-// }
-    
-// })
-document.getElementsByClassName("all").style = "display:flex";
-function selectTab(elll, tabsid) {
-    var i, tabs, tabimgs;
-    
-    tabimgs= document.getElementsByClassName('tabimg');
-
-     for(i=0; i<tabimgs.length; i++){
-        
-         tabimgs[i].style.display='none';
-     };
-
-    tabs=document.getElementsByClassName('tab');
-    for(i=0; i<tabs.length; i++){
-         var buttonvalue=tabs[i].getAttribute('data-filter');
-         if(buttonvalue==tabs[i].className){
-        tabs[i].className= tabs[i].className.replace('active', '');
-        tabimgs[i].style.display='block';
-        console.log(tabs[i])
-
-         }
-    };
-    
-    elll.target.classList.add('active');
-     tabsid.style='display:flex';
-    
-    console.log(tabsid)
-
-};
-
-
+$(document).ready(function(){
+    $(".tab").click(function(){
+        var buttonvalue= $(this).attr("data-filter");       
+        if(buttonvalue =="all"){
+            $(".tabimg").show("1000");
+        }
+        else{
+            $(".tabimg").not("." + buttonvalue).hide("1000");
+            $(".tabimg").filter("." + buttonvalue).show("1000");
+        }
+    });
+    $(".tab").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+});
